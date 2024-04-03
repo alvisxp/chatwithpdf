0236e0a7f1fe455ccdc4e26816783d32ae8553de
alvisxp <128601501+alvisxp@users.noreply.github.com>
2024-04-03 12:09:23 -0400

    Add conversation chain and handle user input

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.embeddings import HuggingFaceInstructEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+from htmlTemp import css, bot_template, user_template
+from langchain_community.llms import HuggingFaceHub
 
 def get_pdf_text(pdf_docs):
     text = " "
@@ -30,15 +34,45 @@ def get_vectorstore(text_chunks):
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
+def get_conversation_chain(vectorstore):
+    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+
 
 
 
 def main():
     load_dotenv()
     st.set_page_config(page_title="Chat with PDFs", page_icon = ":books:")
+    
+    st.write(css, unsafe_allow_html=True)
+
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
 
     st.header("Chat with PDFs :books:")
-    st.text_input("Ask me anything about your PDFs:")
+    user_question = st.text_input("Ask me anything about your PDFs:")
+    if user_question:
+        handle_userinput(user_question)
 
     with st.sidebar:
         st.subheader("Your documents")
@@ -53,9 +87,9 @@ def main():
                 
                 # Vector Store
                 vectorstore = get_vectorstore(text_chunks)
-                print(vectorstore) 
-
-
+                
+                # conversation chain
+                st.session_state.conversation = get_conversation_chain(vectorstore)
 
                  
 
